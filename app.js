0cfee0b9f5a7fb1cddf8477c6ca48c4d31bee122
Aniruddh Mishra <mishraniruddh99@gmail.com>
2022-04-26 00:37:11 +0530

    Removed use createindex from mongo.connect

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const message=error.message;
 const data=error.data;
 return res.status(status).json({"message":message,"data":data})
 });
-mongoose.connect(toString(process.env.MONGO_URL)).then(result=>{
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+  }).then(result=>{
 console.log("Connected")    
 app.listen(process.env.PORT,()=>{
     console.log(`Listening to port ${process.env.PORT}`)
