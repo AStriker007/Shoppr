18c0f130fe744f96b52f90cb4a5e65403df2ae7c
Aniruddh Mishra <mishraniruddh99@gmail.com>
2022-04-25 23:41:49 +0530

    Made chanegs in mongo url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const message=error.message;
 const data=error.data;
 return res.status(status).json({"message":message,"data":data})
 });
-mongoose.connect(process.env.MONGO_URL).then(result=>{
+mongoose.connect(toString(process.env.MONGO_URL)).then(result=>{
 console.log("Connected")    
 app.listen(process.env.PORT,()=>{
     console.log(`Listening to port ${process.env.PORT}`)
