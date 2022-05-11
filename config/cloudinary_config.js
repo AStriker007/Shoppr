18f3490c6062527fc9eb7af9eb11984bec6973d9
Aniruddh Mishra <mishraniruddh99@gmail.com>
2022-05-11 17:43:16 +0530

    Configured cloudinary

---
+++ config/cloudinary_config.js
@@ -0,0 +1,9 @@
+const cloudinary=require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name:'dltndjrdf',
+    api_key:'848464298155846',
+    api_secret:'n9RwkilGchl_RK5wvvM6ACzzu6s'
+})
+
+module.exports=cloudinary
